b6907b046553759d07f9863091079841b50ed62f
food-always-food <16942117+food-always-food@users.noreply.github.com>
2021-08-30 23:33:31 +0100

    Setup Simple Enigma Encrypt Module

--- encrypt.py
+++ encrypt.py
@@ -1,25 +1,26 @@
 from enigma.machine import EnigmaMachine
 import re
 
-machine = EnigmaMachine.from_key_sheet(
-    rotors='I II III',
-    reflector="B",
-    ring_settings='1 1 1',
-    plugboard_settings='AV BS CG DL FU HZ IN KM OW RX'
-)
-
-def process_text(text):
+def clean_text(text):
     regex = re.compile('[^a-zA-Z]')
     punc_free = regex.sub('', text)
     space_free = punc_free.replace(" ", "")
     return(space_free.upper())
 
-machine.set_display("UYT")
+def encryptMessage(text, rotorPositions):
+    machine = EnigmaMachine.from_key_sheet(
+    rotors=rotorPositions,
+    reflector="B",
+    ring_settings='1 1 1',
+    plugboard_settings='AV BS CG DL FU HZ IN KM OW RX'
+    )
+    machine.set_display("UYT")
+    msg_key = machine.process_text('PWR')
+    ciphertext = machine.process_text(clean_text(text))
+    return ciphertext
 
-msg_key = machine.process_text('PWE')
-print(msg_key)
 
-plaintext = "Test the Notes Poot poot boot WHTBEDFU"
-# plaintext = 'THISXISXWORKING'
-ciphertext = machine.process_text(process_text(plaintext))
-print(ciphertext)
+
+# text = encryptMessage("September Fourth, twenty twenty-one Welcome to the party, this is my design. I love you!","I II III")
+
+# print(text)
